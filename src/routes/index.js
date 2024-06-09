9b7a243676215d1bdf56bf3bbddd739d449dd870
eunoiamo <miftahuda701@gmail.com>
2024-06-09 23:16:08 +0700

    add volunteer

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,13 @@ const blogRoute = require('./blogRoute');
 const userRoute = require('./userRoute');
 const wasteRoute = require('./wasteRoute');
 const wasteitemRoute = require('./wasteitemRoute');
+const volunteerRoute = require('./volunteerRoute');
 
 router.use('/events', eventRoute);
 router.use('/blogs', blogRoute);
 router.use('/users', userRoute);
 router.use('/wastes', wasteRoute);
 router.use('/wasteItems', wasteitemRoute);
+router.use('/volunteer', volunteerRoute);
 
 module.exports = router;
