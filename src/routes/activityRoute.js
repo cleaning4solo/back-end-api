aceb34fbebd2d4cd2d9fc6b105676cedf63589e5
Darren Oswaldo Tanjaya <darrenoswaldotanjaya@Darrens-MacBook-Pro.local>
2024-06-17 11:47:45 +0700

    refacture api for activity

--- src/routes/activityRoute.js
+++ src/routes/activityRoute.js
@@ -10,7 +10,7 @@ router.post('/', authenticateToken, createActivity);
 router.post('/:activityId/waste', authenticateToken, addWasteToActivity);
 router.get('/wastes/byActivity/:activityId', authenticateToken, getWastesByActivityId);
 router.patch('/:id/status', updateActivityStatus);
-router.get('/:id', authenticateToken, fetchActivityDetails);
+router.get('/:id', fetchActivityDetails);
 router.get('/', getAllActivities);
 
 module.exports = router;
