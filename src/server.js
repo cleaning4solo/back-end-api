3dc2bd954efb4f24453c8a880ec078a950caef92
eunoiamo <miftahuda701@gmail.com>
2024-06-17 20:43:02 +0700

    last update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const admin = require('firebase-admin');
 require('dotenv').config();
 
 const database = require('./config/database');
